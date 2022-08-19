226ab687a389f1e0b60089ec78fe2db21192c7d8
iAmHeavyCavalry <mathiasanthonyvisser@gmail.com>
2022-08-19 19:50:12 +0200

    passord system
    new miniproject

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+
+        String password = "mathias";
+        String pass = "";
+
+        while(!password.equals(pass)){
+
+            pass = JOptionPane.showInputDialog("Password: ");
+            if(!password.equals(pass)) {
+                System.out.println("Invalid password. Access denied");
+            }
+        }
+        System.out.println("Correct password. Access granted");
+    }
+}
